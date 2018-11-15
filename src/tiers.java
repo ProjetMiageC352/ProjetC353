3d8fa27e122ac87bd84d0d5d760a0a629704e016
Julien <sutre.julien@gmail.com>
2018-11-15 22:50:40 +0100

    Create tiers.java

---
+++ src/tiers.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class tiers extends client {
+
+    /**
+     * Default constructor
+     */
+    public tiers() {
+    }
+
+    /**
+     * 
+     */
+    public void Nom;
+
+    /**
+     * 
+     */
+    public void Prenom;
+
+    /**
+     * 
+     */
+    public void N° Compte;
+
+    /**
+     * 
+     */
+    public void Actif?;
+
+    /**
+     * 
+     */
+    public void CréerTiers() {
+        // TODO implement here
+    }
+
+}
