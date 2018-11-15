1a87708059c52458de25d8115cc760859e1f530d
Julien <sutre.julien@gmail.com>
2018-11-15 22:53:57 +0100

    Create carteBancaire.java

---
+++ src/carteBancaire.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Carte bancaire {
+
+    /**
+     * Default constructor
+     */
+    public Carte bancaire() {
+    }
+
+    /**
+     * 
+     */
+    public void N° carte;
+
+    /**
+     * 
+     */
+    public void type carte;
+
+    /**
+     * 
+     */
+    public void échéance;
+
+    /**
+     * 
+     */
+    public void code crypto;
+
+
+}
