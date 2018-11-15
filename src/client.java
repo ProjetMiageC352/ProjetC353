8cbb5a1bff2c9482e21dab69d3b4018d2fcd8aa8
Julien <sutre.julien@gmail.com>
2018-11-15 22:50:10 +0100

    Create client.java

---
+++ src/client.java
@@ -0,0 +1,74 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class client {
+
+    /**
+     * Default constructor
+     */
+    public client() {
+    }
+
+    /**
+     * 
+     */
+    public void Nom;
+
+    /**
+     * 
+     */
+    public void prénom;
+
+    /**
+     * 
+     */
+    public void adresse mail;
+
+    /**
+     * 
+     */
+    public void type client;
+
+
+
+
+
+    /**
+     * 
+     */
+    public void LierCpteClient() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void LierOpCpte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void ClientAgency?() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CréerCompteAgency() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void LierOpClient() {
+        // TODO implement here
+    }
+
+}
