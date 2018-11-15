69b5918031a2a7bbaa5b5c5bdde5f525a3acce7a
Julien <sutre.julien@gmail.com>
2018-11-15 22:54:10 +0100

    Create operation.java

---
+++ src/operation.java
@@ -0,0 +1,85 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class opération {
+
+    /**
+     * Default constructor
+     */
+    public opération() {
+    }
+
+    /**
+     * 
+     */
+    public void N° opération;
+
+    /**
+     * 
+     */
+    public void date;
+
+    /**
+     * 
+     */
+    public void signe;
+
+    /**
+     * 
+     */
+    public void Type opération;
+
+
+
+    /**
+     * 
+     */
+    public opération est liée à;
+
+
+    /**
+     * 
+     */
+    public void ListerOpération() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void EffectuerVersement() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Date?() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Montant?() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void VerserSolde() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Créer_operation() {
+        // TODO implement here
+    }
+
+}
