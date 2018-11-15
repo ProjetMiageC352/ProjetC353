6fe383a57060780d88a762dd69fd9b149f6b5963
Julien <sutre.julien@gmail.com>
2018-11-15 22:50:12 +0100

    Create compte.java

---
+++ src/compte.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class compte {
+
+    /**
+     * Default constructor
+     */
+    public compte() {
+    }
+
+    /**
+     * 
+     */
+    public void n° compte;
+
+    /**
+     * 
+     */
+    public void type compte;
+
+    /**
+     * 
+     */
+    public void découvert ?;
+
+    /**
+     * 
+     */
+    public void Solde;
+
+
+
+
+    /**
+     * 
+     */
+    public void CréerCompte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void ListeOper?() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void LierOpCpte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void LierOperClient() {
+        // TODO implement here
+    }
+
+}
