0e81f02b0c813798dd7158751293354ce6a636fc
Julien <sutre.julien@gmail.com>
2018-11-15 22:50:33 +0100

    Create interne.java

---
+++ src/interne.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class interne extends client {
+
+    /**
+     * Default constructor
+     */
+    public interne() {
+    }
+
+    /**
+     * 
+     */
+    public void login;
+
+    /**
+     * 
+     */
+    public void mot de passe;
+
+    /**
+     * 
+     */
+    public void type client;
+
+    /**
+     * 
+     */
+    public void année arrivée;
+
+    /**
+     * 
+     */
+    public void N° contrat;
+
+    /**
+     * 
+     */
+    public void agency;
+
+    /**
+     * 
+     */
+    public void N° portable;
+
+
+}
