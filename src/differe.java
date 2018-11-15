719023530b21554cdcca50060e7c5cf3f7fbdff9
Julien <sutre.julien@gmail.com>
2018-11-15 22:54:00 +0100

    Create differe.java

---
+++ src/differe.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class différé extends opération {
+
+    /**
+     * Default constructor
+     */
+    public différé() {
+    }
+
+    /**
+     * 
+     */
+    public void Echéance;
+
+}
