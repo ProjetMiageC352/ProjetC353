73ca72c99bf85732f784c8461c2791cfcdbf2506
Julien <sutre.julien@gmail.com>
2018-11-15 22:50:30 +0100

    Create Interface.java

---
+++ src/Interface.java
@@ -0,0 +1,85 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Interface {
+
+    /**
+     * Default constructor
+     */
+    public Interface() {
+    }
+
+    /**
+     * 
+     */
+    public void Attendre24h() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void FournirRenseignement() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void DemanderRelevéOpération() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void AfficherListe() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CréerCompte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CréerCompteAgency() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void ClôturerCompte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CalculerIntérêts() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CalculerImpôts() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Créer_virement() {
+        // TODO implement here
+    }
+
+}
