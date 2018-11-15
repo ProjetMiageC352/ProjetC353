73590bbe1666942c53d0662ae1626f68cbc938ab
Julien <sutre.julien@gmail.com>
2018-11-15 22:54:07 +0100

    Create ensClient.java

---
+++ src/ensClient.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Ens. Client {
+
+    /**
+     * Default constructor
+     */
+    public Ens. Client() {
+    }
+
+    /**
+     * 
+     */
+    public void Existe?() {
+        // TODO implement here
+    }
+
+}
