877a2371c90c540b12670834c1bd23011dfce228
a-pitet <adeline.pitet@get-mail.fr>
2018-11-17 13:33:14 +0100

    Classe Banque
    Création de la classe Banque.java

--- src/banque.java
+++ src/banque.java
@@ -1,27 +1,114 @@
-
-import java.util.*;
+//package ;
 
 /**
- * 
+ * Classe Banque
  */
-public class banque {
+public class Banque {
 
+	/* **************************** */
+	/*           ATTRIBUTS          */
+	/* **************************** */
+	
+	/**
+	 * Id de la banque.
+	 */
+	protected int id = 0;
+	/**
+	 * Nom de la banque.
+	 */
+	protected String nom = "";
+	/**
+	 * Adresse de la banque.
+	 */
+	protected String adresse = "";
+	
+	/* **************************** */
+	/*         CONSTRUCTEURS        */
+	/* **************************** */
+	
     /**
-     * Default constructor
+     * Constructeur par défaut de la classe.
      */
-    public banque() {
+    public Banque() {
     }
+    /**
+     * Constructeur de la classe.
+     *
+     * @param nomB Nom de la banque
+     * @param adresseB Adresse de la banque
+     */
+    public Banque(final String nomB, final String adresseB) {
+    	this.nom = nomB;
+    	this.adresse = adresseB;
+    }
+    /**
+     * Constructeur de la classe.
+     *
+     * @param idB Id de la banque
+     * @param nomB Nom de la banque
+     * @param adresseB Adresse de la banque
+     */
+    public Banque(final int idB, final String nomB, final String adresseB) {
+        this.id = idB;
+        this.nom = nomB;
+    	this.adresse = adresseB;
+    }
+    
+    /* **************************** */
+	/*            GETTERS           */
+	/* **************************** */
 
     /**
-     * 
+     * Getter de l'd.
+     *
+     * @return id
      */
-    public void Nom;
+    public final int getId() {
+        return id;
+    }
+    /**
+     * Getter du nom.
+     *
+     * @return nom
+     */
+    public final String getNom() {
+        return nom;
+    }
+    /**
+     * Getter de l'adresse.
+     *
+     * @return adresse
+     */
+    public final String getAdresse() {
+        return adresse;
+    }
+    
+    /* **************************** */
+	/*            SETTERS           */
+	/* **************************** */
 
     /**
-     * 
+     * Setter de l'id.
+     *
+     * @param idB Nouvel id
      */
-    public void Adresse;
-
-
-
+    public final void setId(final int idB) {
+        this.id = idB;
+    }
+    /**
+     * Setter du nom.
+     *
+     * @param nomB Nouveau nom
+     */
+    public final void setNom(final String nomB) {
+        this.nom = nomB;
+    }
+    /**
+     * Setter de l'adresse.
+     *
+     * @param adresseB Nouvelle adresse
+     */
+    public final void setAdresse(final String adresseB) {
+        this.adresse = adresseB;
+    }
 }
